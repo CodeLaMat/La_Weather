f6edfb478bff384b769fb4cde140b606f36bf779
CodeLaMat <eyvaz.alishov@gmail.com>
2024-10-02 16:25:32 +0300

    Refactor global styles and add theme toggle to Navigation

--- weather-app-frontend/components/Navigation.tsx
+++ weather-app-frontend/components/Navigation.tsx
@@ -8,12 +8,18 @@ import {
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import { Input } from "@/components/ui/input";
-import { Search } from "lucide-react";
+import { Moon, Search, Sun, UserCircle } from "lucide-react";
 import { useDispatch } from "react-redux";
 import { fetchWeather } from "../thunks/fetchWeather";
 import { AppDispatch } from "../store/store";
 import Link from "next/link";
 import { CardTitle } from "./ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 interface NavigationProps {
   toggleTheme: () => void;
@@ -25,7 +31,7 @@ const Navigation: React.FC<NavigationProps> = ({
   isDarkTheme,
 }) => {
   const [city, setCity] = useState("");
-  const dispatch = useDispatch<AppDispatch>(); 
+  const dispatch = useDispatch<AppDispatch>();
 
   const handleSearch = () => {
     if (city) {
@@ -56,36 +62,60 @@ const Navigation: React.FC<NavigationProps> = ({
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
-      <NavigationMenu className="mx-10">
-        <NavigationMenuList className="flex items-center justify-between w-full">
-          <NavigationMenuItem className="flex-1">
-            <div className="relative w-full max-w-md">
-              <Input
-                type="text"
-                placeholder="Search for city..."
-                className="pl-10"
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
-                onKeyDown={handleKeyDown}
-              />
-              <Search
-                className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer"
-                onClick={handleSearch}
-              />
-            </div>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <div className="flex justify-between">
+        <NavigationMenu className="mx-10">
+          <NavigationMenuList className="flex items-center justify-between w-full">
+            <NavigationMenuItem className="flex-1">
+              <div className="relative w-full max-w-md">
+                <Input
+                  type="text"
+                  placeholder="Search for city..."
+                  className="pl-10"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                />
+                <Search
+                  className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer"
+                  onClick={handleSearch}
+                />
+              </div>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
 
-      <NavigationMenu className="mx-10">
-        <NavigationMenuList className="flex items-center justify-between w-full">
-          <NavigationMenuItem>
-            <button onClick={toggleTheme} className="ml-4">
-              {isDarkTheme ? "🌙" : "☀️"}
-            </button>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+        <NavigationMenu className="mx-10 ">
+          <NavigationMenuList className="flex items-center align-middle justify-between w-full">
+            <NavigationMenuItem>
+              <button onClick={toggleTheme} className="ml-4">
+                {isDarkTheme ? (
+                  <Moon className="h-8 w-8 text-gray-500 hover:text-gray-800" />
+                ) : (
+                  <Sun className="h-8 w-8 text-yellow-500 hover:text-gray-800" />
+                )}
+              </button>
+            </NavigationMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <button className="ml-6">
+                  <UserCircle className="h-8 w-8 text-gray-500 hover:text-gray-800" />
+                </button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>
+                  <Link href="/login">Login/Logout</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/favorites">Favorite Locations</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/history">Search History</Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
     </>
   );
 };
--- weather-app-frontend/styles/themes.ts
+++ weather-app-frontend/styles/themes.ts
@@ -2,10 +2,14 @@ export const lightTheme = {
   body: "#f4f7fc",
   sidebarBg: "#fff",
   cardBg: "#fff",
+  toggleBorder: "#FFF",
+  background: "#363537",
 };
 
 export const darkTheme = {
   body: "#2c2c2c",
   sidebarBg: "#333",
   cardBg: "#444",
+  toggleBorder: "#6B8096",
+  background: "#999",
 };
