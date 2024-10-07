2956e926613d8e6031851222ab831ceb8ce1c59b
CodeLaMat <eyvaz.alishov@gmail.com>
2024-10-07 13:03:06 +0300

    Refactor WeatherDisplay component to toggle between Celsius and Fahrenheit temperatures

--- weather-app-frontend/components/WeatherDisplay.tsx
+++ weather-app-frontend/components/WeatherDisplay.tsx
@@ -6,6 +6,8 @@ import { RootState } from "../store/store";
 import { FiMapPin } from "react-icons/fi";
 
 const kelvinToCelsius = (kelvin: number) => (kelvin - 273.15).toFixed(1);
+const celsiusToFahrenheit = (celsius: number) =>
+  ((celsius * 9) / 5 + 32).toFixed(1);
 
 const WeatherDisplay = () => {
   const [isCelsius, setIsCelsius] = useState(true);
@@ -16,38 +18,45 @@ const WeatherDisplay = () => {
   const loading = useSelector((state: RootState) => state.weather.loading);
   const error = useSelector((state: RootState) => state.weather.error);
 
-  const toggleTemperature = () => {
-    setIsCelsius(!isCelsius);
-  };
+  const toggleTemperature = () => setIsCelsius(!isCelsius);
 
-  if (loading)
-    return <p className=" text-darkText dark:text-lightText">Loading...</p>;
-  if (error)
-    return <p className=" text-darkText dark:text-lightText">Error: {error}</p>;
-  if (!weatherData)
+  if (loading) {
+    return <p className="text-darkText dark:text-lightText">Loading...</p>;
+  }
+  if (error) {
+    return <p className="text-darkText dark:text-lightText">Error: {error}</p>;
+  }
+  if (!weatherData) {
     return (
-      <p className=" text-darkText dark:text-lightText">
-        Location not selected
-      </p>
+      <p className="text-darkText dark:text-lightText">Location not selected</p>
     );
+  }
 
   const weather = weatherData.list[0];
   const temperatureCelsius = kelvinToCelsius(weather.main.temp);
   const feelsLikeCelsius = kelvinToCelsius(weather.main.feels_like);
+  const temperatureMinCelsius = kelvinToCelsius(weather.main.temp_min);
   const weatherDescription = weather.weather[0].description;
   const date = new Date(weather.dt_txt);
-  const temperatureFahrenheit = (
-    ((weather.main.temp - 273.15) * 9) / 5 +
-    32
-  ).toFixed(1);
+
+  const temperature = isCelsius
+    ? `${temperatureCelsius}°C`
+    : `${celsiusToFahrenheit(parseFloat(temperatureCelsius))}°F`;
+
+  const feelsLike = isCelsius
+    ? `${feelsLikeCelsius}°C`
+    : `${celsiusToFahrenheit(parseFloat(feelsLikeCelsius))}°F`;
+
+  const temperatureMin = isCelsius
+    ? `${temperatureMinCelsius}°C`
+    : `${celsiusToFahrenheit(parseFloat(temperatureMinCelsius))}°F`;
 
   const iconCode = weather.weather[0].icon;
   const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
   return (
-    <div className="p-2 max-w-md mx-auto relative  text-darkText dark:text-lightText">
-      {/* Location and Date */}
-      <div className="flex justify-between items-center mb-4  text-darkText dark:text-lightText">
+    <div className="p-2 max-w-md mx-auto relative text-darkText dark:text-lightText">
+      <div className="flex justify-between items-center mb-4 text-darkText dark:text-lightText">
         <div className="flex items-center space-x-2 text-darkText dark:text-lightText">
           <span className="dark:bg-darkBody rounded-full py-2 px-4 text-sm flex gap-2">
             <FiMapPin className="text-xl" />
@@ -62,7 +71,6 @@ const WeatherDisplay = () => {
         </button>
       </div>
 
-      {/* Main Weather Details */}
       <div className="flex justify-between items-center">
         <div>
           <h1 className="text-3xl 2xl:text-4xl font-semibold text-darkText dark:text-lightText">
@@ -71,28 +79,21 @@ const WeatherDisplay = () => {
           <p className="text-gray-400 text-sm">{date.toLocaleDateString()}</p>
         </div>
 
-        {/* Right section (Temperature) */}
         <div className="text-right">
-          <p className="text-4xl font-semibold  text-darkText dark:text-lightText">
-            {isCelsius
-              ? `${temperatureCelsius}°C`
-              : `${temperatureFahrenheit}°F`}
+          <p className="text-4xl font-semibold text-darkText dark:text-lightText">
+            {temperature}
           </p>
-          <p className="text-gray-400">
-            /{kelvinToCelsius(weather.main.temp_min)}°C
-          </p>
+          <p className="text-gray-400">/ {temperatureMin}</p>
         </div>
       </div>
 
-      {/* Middle Section (Weather Icon) */}
       <div className="flex justify-center items-center my-2">
-        <img src={iconUrl} alt="Weather icon" className="w-28 h-28 " />
+        <img src={iconUrl} alt="Weather icon" className="w-28 h-28" />
       </div>
 
-      {/* Weather Condition at the Bottom Right */}
-      <div className="absolute bottom-2 right-2 text-right  text-darkText dark:text-lightText">
+      <div className="absolute bottom-2 right-2 text-right text-darkText dark:text-lightText">
         <p className="text-lg font-medium capitalize">{weatherDescription}</p>
-        <p className="text-gray-400 text-sm">Feels like {feelsLikeCelsius}°C</p>
+        <p className="text-gray-400 text-sm">Feels like {feelsLike}</p>
       </div>
     </div>
   );
