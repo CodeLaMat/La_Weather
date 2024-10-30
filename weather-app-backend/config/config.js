29364e95e1a665fafaa6e7fa73acc78f652b099d
CodeLaMat <eyvaz.alishov@gmail.com>
2024-10-30 21:40:05 +0200

    Remove backend folder from frontend branch

--- weather-app-backend/config/config.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || "defaultSecret",
-  mongoURI: process.env.MONGO_URI,
-  port: process.env.PORT || 5000,
-};
